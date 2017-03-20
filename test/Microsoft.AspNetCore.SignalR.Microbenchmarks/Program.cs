9aaf81cc699bae346f0125e6dd67f040cb1662a6
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-03-20 10:11:57 -0700

    add some very simple, fairly stupid, benchmarks

---
+++ test/Microsoft.AspNetCore.SignalR.Microbenchmarks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using BenchmarkDotNet.Running;
+
+namespace Microsoft.AspNetCore.SignalR.Microbenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Contains("--debug"))
+            {
+                args = args.Where(a => a != "--debug").ToArray();
+                Console.WriteLine($"Waiting for debugger to attach. Process ID: {Process.GetCurrentProcess().Id} ");
+                Console.WriteLine("Press ENTER to continue");
+                Console.ReadLine();
+            }
+            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+
+            //var bench = new MessageParserBenchmarks() { ChunkSize = 0, Format = Sockets.MessageFormat.Binary };
+            //bench.Setup();
+            //bench.SingleTextMessageCopy();
+        }
+    }
+}
