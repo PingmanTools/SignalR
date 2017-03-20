51d5036ea05d5f87f2b11d13afc9e72ccbe2abf2
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-03-20 13:51:37 -0700

    pr feedback

--- test/Microsoft.AspNetCore.SignalR.Microbenchmarks/Program.cs
+++ test/Microsoft.AspNetCore.SignalR.Microbenchmarks/Program.cs
@@ -10,13 +10,6 @@ namespace Microsoft.AspNetCore.SignalR.Microbenchmarks
     {
         static void Main(string[] args)
         {
-            if (args.Contains("--debug"))
-            {
-                args = args.Where(a => a != "--debug").ToArray();
-                Console.WriteLine($"Waiting for debugger to attach. Process ID: {Process.GetCurrentProcess().Id} ");
-                Console.WriteLine("Press ENTER to continue");
-                Console.ReadLine();
-            }
             BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
         }
     }
